41b78590b3c2a670bfc335d34b337e3942aec70d
Tejas Rajopadhye <trajop2@uic.edu>
2021-10-22 13:24:28 -0500

    Added Some Tests for Db Operations

---
+++ Assignment_2_Backend/tests/userOperationsDB.spec.js
@@ -0,0 +1,21 @@
+dbOperations = require("../models/userOperations")
+
+describe("Tests for user CRUD operatiosn ", ()=>{
+
+    test("Test for Create Operation", ()=>{
+
+        userObj = {
+            
+                "username" : "Tejas ee",
+                "dob" : "10-19-2000",
+                "emailId" : "tejas22@gmail.com",
+                "password" : "password@123" 
+        }
+
+        dbOperations.insertNewUser(userObj, function(err, data){
+            expect(data).toBe(true)
+        })
+    })
+
+
+})
