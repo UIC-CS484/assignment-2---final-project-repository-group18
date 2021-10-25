ca98d519540bd5cf4541dcc0d2214a2f904b4111
Tejas Rajopadhye <trajop2@uic.edu>
2021-10-25 16:19:42 -0500

    Commenting user operations unit tests

--- Assignment_2_Backend/tests/userOperationsDB.spec.js
+++ Assignment_2_Backend/tests/userOperationsDB.spec.js
@@ -1,23 +1,23 @@
-// TESTS IN PROCESS. NOT TO BE CONSIDERED FOR ASSIGNMENT 2
+// // TESTS IN PROCESS. NOT TO BE CONSIDERED FOR ASSIGNMENT 2
 
-dbOperations = require("../models/userOperations")
+// dbOperations = require("../models/userOperations")
 
-describe("Tests for user CRUD operatiosn ", ()=>{
+// describe("Tests for user CRUD operatiosn ", ()=>{
 
-    test("Test for Create Operation", ()=>{
+//     test("Test for Create Operation", ()=>{
 
-        userObj = {
+//         userObj = {
             
-                "username" : "Tejas ee",
-                "dob" : "10-19-2000",
-                "emailId" : "tejas22@gmail.com",
-                "password" : "password@123" 
-        }
+//                 "username" : "Tejas ee",
+//                 "dob" : "10-19-2000",
+//                 "emailId" : "tejas22@gmail.com",
+//                 "password" : "password@123" 
+//         }
 
-        dbOperations.insertNewUser(userObj, function(err, data){
-            expect(data).toBe(true)
-        })
-    })
+//         dbOperations.insertNewUser(userObj, function(err, data){
+//             expect(data).toBe(true)
+//         })
+//     })
 
 
-})
+// })
