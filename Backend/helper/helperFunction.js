c1235fba7d521f9c8decc8218be2d2f6cb27d581
Tejas Rajopadhye <trajop2@uic.edu>
2021-11-11 21:40:35 -0600

    Restructured the code

---
+++ Backend/helper/helperFunction.js
@@ -0,0 +1,56 @@
+const PASSWORDLENGTH = 8 // TODO Change the password Length from hardcoded to taken from config file
+
+
+function checkIfAllDetailsPresent(req, callback){
+    // if (req.body.userId == null || req.body.userId == ''){
+    //     err = "UserId is empty "
+    //     callback(err, null)
+    // }
+    //userId = req.body.userId
+    if (req.body.emailId == null || req.body.emailId == ''){
+        err = "Email ID is empty "
+        return callback(err, null)   
+    }
+    emailId = req.body.emailId
+    // Check if email Id is in correct form
+    // Trim the white space 
+    emailId = emailId.trim()
+    if (req.body.username == null || req.body.username == ''){
+        err = "User Name is empty"
+        return callback(err, null)
+    }
+    username = req.body.username
+    if (req.body.password == null || req.body.password == ''){
+        err = "Password is empty"
+        return callback(err, null)
+    }
+    password = req.body.password
+
+    
+    if(req.body.dob == null || req.body.dob == ''){
+        err = "Dob is empty"
+        return callback(err, null)
+    }
+    dob = req.body.dob
+
+
+    dataObj = {
+        // userId : userId,
+        emailId : emailId,
+        username : username,
+        password : password,
+        dob : dob 
+    }
+    return callback(null, dataObj)
+}
+
+function checkPasswordLength(data){
+    if(data.password.length < PASSWORDLENGTH){
+        return false
+    }
+
+    return true
+}
+
+exports.checkIfAllDetailsPresent = checkIfAllDetailsPresent
+exports.checkPasswordLength = checkPasswordLength
