f675f3d492c31e83ca158ce24921035d19501b31
Tejas Rajopadhye <trajop2@uic.edu>
2021-11-11 21:31:49 -0600

    Deleted repitative things and added env files

--- Assignment_2_Backend/tests/userOperationsDB.spec.js
+++ Assignment_2_Backend/tests/userOperationsDB.spec.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // // TESTS IN PROCESS. NOT TO BE CONSIDERED FOR ASSIGNMENT 2
 
 // dbOperations = require("../models/userOperations")
@@ -22,21 +21,3 @@
 
 
 // })
-=======
-// TESTS IN PROCESS. NOT TO BE CONSIDERED FOR ASSIGNMENT 2
-
-const userController = require("../controller/userController")
-
-describe("Tests for user CRUD operatiosn ", ()=>{
-
-    test("Create a user and check the reponse", function(){
-        userCreationObj = {
-                "emailId": "test12@test.com",
-                "password": "password@123",
-                "username":"T r",
-                "dob" : "12/10/1921"
-        }
-    })
-
-})
->>>>>>> 54f7790861704bde9231917200c31d853c886ca6
