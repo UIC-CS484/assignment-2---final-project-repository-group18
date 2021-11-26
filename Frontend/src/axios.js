da4ddf5fc5a8461a263229961801d3b979a23ae2
Tejas Rajopadhye <trajop2@uic.edu>
2021-11-26 17:07:02 -0600

    Added files for frontend CI - 1

--- Frontend/src/axios.js
+++ Frontend/src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:1337",
+  //baseURL: "http://localhost:1337",
+  baseURL: "https://swadbackendapp.herokuapp.com",
 });
 
 instance.defaults.withCredentials = true;
